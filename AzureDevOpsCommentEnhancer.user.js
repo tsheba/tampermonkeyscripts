26c55920f4091c04ebabed5d9dfc1397d08228ef
tsheba <69246151+tsheba@users.noreply.github.com>
2020-08-05 15:12:37 +0200

    version

--- AzureDevOpsCommentEnhancer.user.js
+++ AzureDevOpsCommentEnhancer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Fix ADS checkin comments in discussion and history of workitems
-// @version      0.3
+// @version      0.4
 // @author       Tobias Sachs
 //  ... add match eg https://myazerdevops/*
 // @match        https://
